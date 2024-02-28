eef707304ff022639efa8c34b9af0b1b0d60dee7
yifatre <yifat.reg@gmail.com>
2024-02-28 12:40:28 +0200

    Add render meme & setLineTxt

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,1 +1,56 @@
 'use strict'
+
+let gElCanvas
+let gCtx
+
+const TOUCH_EVENTS = ['touchstart', 'touchmove', 'touchend']
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    renderMeme()
+
+
+}
+
+
+function renderMeme() {
+    const meme = getMeme()
+    const img = new Image()
+    img.src = getImgById(meme.selectedImgId).url
+    img.onload = () => {
+        renderImg(img)
+        drawText(meme.lines[0].txt)
+    }
+}
+
+function renderImg(img) {
+    // Adjust the canvas to the new image size
+    gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+
+    // Draw the img on the canvas
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+
+function drawText(text = 'hello', x = 0, y = 0) {
+    gCtx.lineWidth = 1
+    gCtx.strokeStyle = 'black'
+
+    gCtx.fillStyle = 'white'
+
+    gCtx.font = '45px Impact'
+    // gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'top'
+
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+function onTextInput(txt) {
+    setLineText(txt)
+    renderMeme()
+}
+
+
--- js/service/meme.service.js
+++ js/service/meme.service.js
@@ -1,1 +1,29 @@
 'use strict'
+
+var gImgs = [{ id: 4, url: 'img/4.jpg', keywords: ['funny', 'cat'] }, { id: 18, url: 'img/18.jpg', keywords: ['funny', 'cat'] }]
+
+var gMeme = {
+    selectedImgId: 4,
+    selectedLineIdx: 0,
+    lines: [{ txt: 'I sometimes eat Falafel', size: 20, color: 'red' }]
+}
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+
+function getMeme() {
+    return gMeme
+}
+
+function getImgs() {
+    return gImgs
+}
+
+function getImgById(id) {
+    return gImgs.find(img => img.id === id)
+}
+
+
+
+function setLineText(txt, lineIdx = 0) {
+    gMeme.lines[lineIdx].txt = txt
+}
