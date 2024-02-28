c9d4d563c3b7d4d457912cee9b55a673d660e6a3
yifatre <yifat.reg@gmail.com>
2024-02-28 13:55:17 +0200

    Submission Wednesday 14_00 -- Add download & color picker

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -14,7 +14,7 @@ function renderMeme() {
     img.src = getImgById(meme.selectedImgId).url
     img.onload = () => {
         renderImg(img)
-        drawText(meme.lines[0].txt)
+        drawText(meme.lines[0])
     }
 }
 
@@ -27,23 +27,43 @@ function renderImg(img) {
 }
 
 
-function drawText(text = 'hello', x = 0, y = 0) {
+function drawText(line, x = 0, y = 0) {
     gCtx.lineWidth = 1
-    gCtx.strokeStyle = 'black'
+    gCtx.strokeStyle = line.color
 
-    gCtx.fillStyle = 'white'
+    gCtx.fillStyle = line.fill
 
-    gCtx.font = '45px Impact'
+    gCtx.font = `${line.size}px Impact`
     // gCtx.textAlign = 'center'
     gCtx.textBaseline = 'top'
 
-    gCtx.fillText(text, x, y)
-    gCtx.strokeText(text, x, y)
+    gCtx.fillText(line.txt, x, y)
+    gCtx.strokeText(line.txt, x, y)
 }
 
 function onTextInput(txt) {
     setLineText(txt)
     renderMeme()
 }
+
+function onDownloadMeme(elLink) {
+    const imgContent = gElCanvas.toDataURL('image/jpeg') // image/jpeg the default format
+    elLink.href = imgContent
+}
+
+function onChangeOutline(color) {
+    setLineColor(color)
+    renderMeme()
+}
+
+function onChangeFill(color) {
+    setLineFill(color)
+    renderMeme()
+}
+
+// function onChangeFontSize(dSize){
+//     setLineSize(dSize)
+//     renderMeme()
+// }
 
 
--- js/service/meme.service.js
+++ js/service/meme.service.js
@@ -5,7 +5,7 @@ var gImgs = [{ id: 4, url: 'img/4.jpg', keywords: ['funny', 'cat'] }, { id: 18, 
 var gMeme = {
     selectedImgId: 4,
     selectedLineIdx: 0,
-    lines: [{ txt: 'I sometimes eat Falafel', size: 20, color: 'red' }]
+    lines: [{ txt: 'I sometimes eat Falafel', size: 20, color: 'red', fill: 'white' }]
 }
 var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
@@ -22,10 +22,23 @@ function getImgById(id) {
     return gImgs.find(img => img.id === id)
 }
 
-function setMemeImg(imgId){
+function setMemeImg(imgId) {
     gMeme.selectedImgId = imgId
 }
 
 function setLineText(txt, lineIdx = 0) {
     gMeme.lines[lineIdx].txt = txt
 }
+
+function setLineColor(color, lineIdx = 0) {
+    gMeme.lines[lineIdx].color = color
+}
+
+function setLineFill(color, lineIdx = 0) {
+    gMeme.lines[lineIdx].fill = color
+}
+
+function setLineSize(dSize, lineIdx = 0) {
+    gMeme.lines[lineIdx].size = dSize
+}
+
